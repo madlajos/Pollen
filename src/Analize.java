fe8336bfe83c1660c2dace38cd6d6412e61df41e
madlajos <madlajos96@gmail.com>
2017-07-12 21:48:25 +0200

    Elso kommit (ujra)
    Jocival hepajkodás

---
+++ src/Analize.java
@@ -0,0 +1,27 @@
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+
+public class Analize {
+	
+	public void browse() {
+		// Mat image = Highgui.imrad("C:/Users/madla/OneDrive/Documents/GitHub/imgproc/VH484.jpg");
+	
+	}
+	
+}
